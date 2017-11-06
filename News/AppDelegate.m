c562d015c3e9764a0e3b1b2cf749ff2761986fc9
liulishuo <liulishuo2017@163.com>
2017-11-06 11:37:57 +0800

    整理代码

--- News/AppDelegate.m
+++ News/AppDelegate.m
@@ -22,11 +22,6 @@
     return YES;
 }
 
-- (void)configureClientInfo {
-    _clientId = @"BtZoBtnOjnUc3tPlkwXs";
-    _clientSecret = @"lMfgxMRZUqGItiEmsgTEddWgNTHqWk4R";
-    _redirectUrl = @"http://www.travelease.com.cn";
-}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -54,5 +49,11 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+#pragma mark - Methods
+- (void)configureClientInfo {
+    _clientId = @"BtZoBtnOjnUc3tPlkwXs";
+    _clientSecret = @"lMfgxMRZUqGItiEmsgTEddWgNTHqWk4R";
+    _redirectUrl = @"http://www.travelease.com.cn";
+}
 
 @end
